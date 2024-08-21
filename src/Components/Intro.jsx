5da97fe1ee9e212993ca30a932d2e468091f0131
Syed Aatif <syedaatif0047@gmail.com>
2024-08-21 21:43:25 +0530

    recent changes to the site

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -4,38 +4,12 @@ function Intro() {
   return ( 
     <> 
     <div className='bg'>
-      <div id="R">
-    <h1 className='a'>Hello, My name is </h1>
-    <h1 className='b'>Syed  Aatif </h1>
-    <h1 className='c'>Full Stack Dev</h1>
-     
-         </div>
-      <div id="L">  </div> 
-     
-    </div>
-    <div className="ab">
+       <div id="L">                     </div>
 
+       <div id="R">                     </div>
 
-
-      
-    </div>
-   <div className="aboutme">
-   <div className="c">
-   <app-article><article class=" p-5 h-full bg-white shadow-2xl rounded-md overflow-hidden hover:scale-105 transition-transform relative"><span class="absolute bg-indigo-100 text-indigo-500 px-2 py-1 left-3 top-3 text-xs rounded font-semibold">  Project   </span><div class="p-6 space-y-2"><h3 class="text-xl font-semibold line-clamp-3"> Super Mario Land, Alleyway and Baseball join Nintendo Switch membership </h3><div class="text-xs font-semibold text-gray-500 flex flex-wrap justify-between"><span>15.05.2024</span><span>Sophie Atkinson</span></div><p class="line-clamp-5">The Nintendo Switch Online subscription library has been expanded once again with the introduction of three classic Game Boy titles.… Continue reading Super Mario Land, Alleyway and Baseball join Nintendo Switch membership
-   The post Super Mario Land, Alleyway …</p><a scrolltotop="" class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" href="#/category/baseball/0"> read more </a></div></article></app-article>
-   </div>
-   <div className="c">
-   <app-article><article class=" p-5 h-full bg-white shadow-2xl rounded-md overflow-hidden hover:scale-105 transition-transform relative"><span class="absolute bg-indigo-100 text-indigo-500 px-2 py-1 left-3 top-3 text-xs rounded font-semibold"> ReadWrite </span><div class="p-6 space-y-2"><h3 class="text-xl font-semibold line-clamp-3"> Super Mario Land, Alleyway and Baseball join Nintendo Switch membership </h3><div class="text-xs font-semibold text-gray-500 flex flex-wrap justify-between"><span>15.05.2024</span><span>Sophie Atkinson</span></div><p class="line-clamp-5">The Nintendo Switch Online subscription library has been expanded once again with the introduction of three classic Game Boy titles.… Continue reading Super Mario Land, Alleyway and Baseball join Nintendo Switch membership
-   The post Super Mario Land, Alleyway …</p><a scrolltotop="" class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" href="#/category/baseball/0"> read more </a></div></article></app-article>
-   </div>
-   <div className="c">
-   <app-article><article class=" p-5 h-full bg-white shadow-2xl rounded-md overflow-hidden hover:scale-105 transition-transform relative"><span class="absolute bg-indigo-100 text-indigo-500 px-2 py-1 left-3 top-3 text-xs rounded font-semibold"> ReadWrite </span><div class="p-6 space-y-2"><h3 class="text-xl font-semibold line-clamp-3"> Super Mario Land, Alleyway and Baseball join Nintendo Switch membership </h3><div class="text-xs font-semibold text-gray-500 flex flex-wrap justify-between"><span>15.05.2024</span><span>Sophie Atkinson</span></div><p class="line-clamp-5">The Nintendo Switch Online subscription library has been expanded once again with the introduction of three classic Game Boy titles.… Continue reading Super Mario Land, Alleyway and Baseball join Nintendo Switch membership
-   The post Super Mario Land, Alleyway …</p><a scrolltotop="" class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" href="#/category/baseball/0"> read more </a></div></article></app-article>
-   </div>
-
-
-        
-   </div>
+       </div> 
+   
    </>
   )
 }
