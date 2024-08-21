7d635961029c783eecacac286f334637a5bf77c1
Syed Aatif <syedaatif0047@gmail.com>
2024-08-21 22:10:23 +0530

    fixed positions of div

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -4,9 +4,22 @@ function Intro() {
   return ( 
     <> 
     <div className='bg'>
-       <div id="L">                     </div>
+       <div id="L">     
+      
+      
+      
+       </div>
 
-       <div id="R">                     </div>
+       <div id="R">     
+        
+
+
+        <h1>hiiiii</h1> <h1>iiiiii</h1> <h1>iiiiii</h1> <h1>iiiiii  </h1>
+        
+        
+        
+        
+                        </div>
 
        </div> 
    
